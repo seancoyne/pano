68ef4c3a036ead8b80ff3ad21aab88f094ede09b
Laurence Lewis <laurence.r.lewis@gmail.com>
2017-10-10 14:25:46 +1000

    Updated to allow for keyboard controls and add ARIA to HTML
    Added
    focusin to $leftCtrl and $right Ctrl
    Added additional div with $("#stopScroll").focus();
    role="region" aria-label="Pano image" to parent container
    changed a href to button element for better semantics
    aria-label="scroll image left"
    aria-label="scroll image right"
    aria-label="Stop"

--- jquery.pano.js
+++ jquery.pano.js
@@ -1,240 +1,245 @@
-/* global jQuery, module, require */
+/* global jQuery */
 
 /*
 
-Pano v1.2.0
+Pano v1.1.0
 jQuery plugin to display a 360 degree panoramic image
 Sean Coyne
+Updated for keyboard and A11Y by Laurence Lewis
 https://github.com/seancoyne/pano
 https://seancoyne.github.io/pano
 
 */
 
-(function (factory) {
-  if(typeof module === "object" && typeof module.exports === "object") {
-    module.exports = factory(require("jquery"));
-  } else {
-    factory(jQuery);
-  }
-}(function($) {
-
-	$.fn.pano = function(options){
+jQuery.fn.pano = function(options){
+	
+	this.version = "1.1.1";
+	
+	// get a handle on the panorama and controls
+	var $pano = this;
+	var $leftCtrl = $pano.find(".controls").find("button.left");
+	var $rightCtrl = $pano.find(".controls").find("button.right");
+	
+	var getImageWidth = function(imgSrc) {
+		var img = new Image();
+		img.src = imgSrc;
+		return img.width;
+	};
+	
+	var moveBackgroundTo = function(newPos, duration, cb) {		
+		duration = duration || 0;
+		cb = cb || function(){};
+		$pano.animate({
+			"background-position": newPos.toString() + "px"
+		}, duration, "linear", cb);
+	};
+	
+	var moveBackgroundBy = function(distance, duration, cb) {
+		duration = duration || 0;
+		cb = cb || function(){};
+		moveBackgroundTo(getCurrentPosition() + distance, duration, cb);
+	};
+	
+	var getCurrentPosition = function() {
+		return parseInt($pano.css("background-position").split(" ")[0].replace("px", ""));
+	};
+	
+	var indicateMovement = function() {
+		$pano.addClass("moving");
+	};
+	
+	var noMovement = function() {
+		$pano.removeClass("moving");
+	};
+	
+	var insideImage = function(mouseXPos) {
+		var $offsetLeft = $pano.offset().left;
+		var maxLeft = $offsetLeft;
+		var maxRight = $offsetLeft + $pano.width();
+		if( mouseXPos < maxLeft || mouseXPos > maxRight) {
+			return false;
+		}
+		return true;
+	};
+	
+	var dragMove = function(xPos, startPosition, cb) {
+	
+		// dont move if you're outside the image
+		if (!insideImage(xPos)) {
+			return false;
+		} 
 		
-		this.version = "1.2.0";
+		// calculate the change in position
+		var diff = (xPos - startPosition);
 		
-		// get a handle on the panorama and controls
-		var $pano = this;
-		var $leftCtrl = $pano.find(".controls").find("a.left");
-		var $rightCtrl = $pano.find(".controls").find("a.right");
-		
-		var getImageWidth = function(imgSrc) {
-			var img = new Image();
-			img.src = imgSrc;
-			return img.width;
-		};
-		
-		var moveBackgroundTo = function(newPos, duration, cb) {		
-			duration = duration || 0;
-			cb = cb || function(){};
-			$pano.animate({
-				"background-position": newPos.toString() + "px"
-			}, duration, "linear", cb);
-		};
-		
-		var moveBackgroundBy = function(distance, duration, cb) {
-			duration = duration || 0;
-			cb = cb || function(){};
-			moveBackgroundTo(getCurrentPosition() + distance, duration, cb);
-		};
-		
-		var getCurrentPosition = function() {
-			return parseInt($pano.css("background-position").split(" ")[0].replace("px", ""));
-		};
-		
-		var indicateMovement = function() {
-			$pano.addClass("moving");
-		};
-		
-		var noMovement = function() {
-			$pano.removeClass("moving");
-		};
-		
-		var insideImage = function(mouseXPos) {
-			var $offsetLeft = $pano.offset().left;
-			var maxLeft = $offsetLeft;
-			var maxRight = $offsetLeft + $pano.width();
-			if( mouseXPos < maxLeft || mouseXPos > maxRight) {
-				return false;
-			}
-			return true;
-		};
-		
-		var dragMove = function(xPos, startPosition, cb) {
-		
-			// dont move if you're outside the image
-			if (!insideImage(xPos)) {
-				return false;
-			} 
-			
-			// calculate the change in position
-			var diff = (xPos - startPosition);
-			
-			// move it
-			moveBackgroundBy(diff, 0, cb);
-			
-		};
-		
-		var leftMover,
-			rightMover,
-			ctrlInterval = options.interval || 100,
-			ctrlSpeed = options.speed || 50;
-			
-		// setup the initial styling
-		$pano.css({
-			"background-image": "url('" + options.img + "')",
-			"background-position": "50% 50%",
-			"background-size": "auto 100%",
-			"background-repeat": "repeat-x"
-		});
-		
-		// set the initial position in pixels (easier math)
-		var halfWidth = (getImageWidth(options.img) / 2);
-		moveBackgroundTo(halfWidth);
-		
-		var moveLeft = function(interval, speed) {
-			
-			interval = interval || ctrlInterval;
-			speed = speed || ctrlSpeed;
-			
-			// indicate movement
-			indicateMovement();
-			
-			// immediately move 
-			moveBackgroundBy(speed, 100);
-			
-			// move left on interval
-			leftMover = setInterval(function(){
-				moveBackgroundBy(speed, 100);
-			}, interval);
-			
-		};
-		
-		var moveRight = function(interval, speed) {
-			
-			interval = interval || ctrlInterval;
-			speed = speed || ctrlSpeed;
-			
-			// indicate movement
-			indicateMovement();
-			
-			// immediately move 
-			moveBackgroundBy(-speed, 100);
-			
-			// move right on interval
-			rightMover = setInterval(function(){
-				moveBackgroundBy(-speed, 100);
-			}, interval);
-		};
-		
-		var stopMoving = function() {
-			$pano.off("touchmove");
-			$pano.off("mousemove");
-			$pano.stop(true, true);
-			clearInterval(leftMover);
-			clearInterval(rightMover);
-			noMovement();
-		};
-		
-		$leftCtrl.on("mousedown", function(event){
-			
-			// dont process the drag events
-			event.stopPropagation();
-			
-			moveLeft();
-			
-			
-		}).on("touchstart", function(event){
-			
-			// dont process the drag events
-			event.stopPropagation();
-			
-			// don't show the context menu while holding
-			event.preventDefault();
-			
-			moveLeft();
-			
-		});
-		
-		$rightCtrl.on("mousedown", function(event){
-			
-			// dont process the drag events
-			event.stopPropagation();
-			
-			moveRight();
-			
-		}).on("touchstart", function(event){
-			
-			// dont process the drag events
-			event.stopPropagation();
-			
-			// don't show the context menu while holding
-			event.preventDefault();
-			
-			moveRight();
-			
-		});
-		
-		$pano.on("mousedown", function(event){
-			
-			// indicate movement
-			indicateMovement();
-			
-			var startPosition = event.pageX;
-			
-			$pano.on("mousemove", function(event){
-				
-				var xPos = event.pageX;
-				dragMove(xPos, startPosition, function(){
-					// after animation is complete, set the "start" position to the current position
-					startPosition =xPos;
-				});
-				
-			});
-			
-		}).on("touchstart", function(event){
-			
-			// indicate movement
-			indicateMovement();
-			
-			// don't show the context menu while holding
-			event.preventDefault();
-			
-			var startPosition = event.pageX;
-			
-			$pano.on("touchmove", function(event){
-				
-				var xPos = event.originalEvent.changedTouches[0].pageX;
-				dragMove(xPos, startPosition, function(){
-					// after animation is complete, set the "start" position to the current position
-					startPosition = xPos;
-				});
-				
-			});
-			
-		});
-		
-		$("body").on("mouseup", function(){
-			stopMoving();
-		}).on("touchend", function(){
-			stopMoving();
-		});
-		
-		return {
-			moveLeft: moveLeft,
-			moveRight: moveRight,
-			stopMoving: stopMoving
-		};
+		// move it
+		moveBackgroundBy(diff, 0, cb);
 		
 	};
+	
+	var leftMover,
+		rightMover,
+		ctrlInterval = options.interval || 100,
+		ctrlSpeed = options.speed || 50;
+		
+	// setup the initial styling
+	$pano.css({
+		"background-image": "url('" + options.img + "')",
+		"background-position": "50% 50%",
+		"background-size": "auto 100%",
+		"background-repeat": "repeat-x"
+	});
+	
+	// set the initial position in pixels (easier math)
+	var halfWidth = (getImageWidth(options.img) / 2);
+	moveBackgroundTo(halfWidth);
+	
+	var moveLeft = function(interval, speed) {
+		
+		interval = interval || ctrlInterval;
+		speed = speed || ctrlSpeed;
+		
+		// indicate movement
+		indicateMovement();
+		
+		// immediately move 
+		moveBackgroundBy(speed, 100);
+		
+		// move left on interval
+		leftMover = setInterval(function(){
+			moveBackgroundBy(speed, 100);
+		}, interval);
+		
+	};
+	
+	var moveRight = function(interval, speed) {
+		
+		interval = interval || ctrlInterval;
+		speed = speed || ctrlSpeed;
+		
+		// indicate movement
+		indicateMovement();
+		
+		// immediately move 
+		moveBackgroundBy(-speed, 100);
+		
+		// move right on interval
+		rightMover = setInterval(function(){
+			moveBackgroundBy(-speed, 100);
+		}, interval);
+	};
+	
+	var stopMoving = function() {
+		$pano.off("touchmove");
+		$pano.off("mousemove");
+		$pano.stop(true, true);
+		clearInterval(leftMover);
+		clearInterval(rightMover);
+		noMovement();
+	};
+	//Add focusin so the left control can be activated using the keyboard
+	$leftCtrl.on("mousedown focusin", function(event){
+		
+		// dont process the drag events
+		event.stopPropagation();
+		
+        moveLeft();
+		
+		
+	}).on("touchstart", function(event){
+		
+		// dont process the drag events
+		event.stopPropagation();
+		
+		// don't show the context menu while holding
+		event.preventDefault();
+		
+		moveLeft();
+		
+	});
+    //Stop the scroll when focus moves away from the left control
+    $leftCtrl.on("focusout", function(event){
+		
+		stopMoving();
 
-}));
+        $("#stopScroll").focus();
+	});
+	//Add focusin so the right control can be activated using the keyboard
+	$rightCtrl.on("mousedown focusin", function(event){
+		
+		// dont process the drag events
+		event.stopPropagation();
+		
+		moveRight();
+		
+	}).on("touchstart", function(event){
+		
+		// dont process the drag events
+		event.stopPropagation();
+		
+		// don't show the context menu while holding
+		event.preventDefault();
+		
+		moveRight();
+		
+	});
+    //Stop the scroll when focus moves away from the left control
+    $rightCtrl.on("focusout", function(event){
+		
+		// dont process the drag events
+		stopMoving();
+        $("#stopScroll").focus();
+	});
+	
+	$pano.on("mousedown", function(event){
+		
+		// indicate movement
+		indicateMovement();
+		
+		var startPosition = event.pageX;
+		
+		$pano.on("mousemove", function(event){
+			
+			var xPos = event.pageX;
+			dragMove(xPos, startPosition, function(){
+				// after animation is complete, set the "start" position to the current position
+				startPosition =xPos;
+			});
+			
+		});
+		
+	}).on("touchstart", function(event){
+		
+		// indicate movement
+		indicateMovement();
+		
+		// don't show the context menu while holding
+		event.preventDefault();
+		
+		var startPosition = event.pageX;
+		
+		$pano.on("touchmove", function(event){
+			
+			var xPos = event.originalEvent.changedTouches[0].pageX;
+			dragMove(xPos, startPosition, function(){
+				// after animation is complete, set the "start" position to the current position
+				startPosition = xPos;
+			});
+			
+		});
+		
+	});
+	
+	jQuery("body").on("mouseup", function(){
+		stopMoving();
+	}).on("touchend", function(){
+		stopMoving();
+	});
+	
+	return {
+		moveLeft: moveLeft,
+		moveRight: moveRight,
+		stopMoving: stopMoving
+	};
+	
+};
